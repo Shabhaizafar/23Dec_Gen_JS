98b9230f67a648f4aa35bb99adf49fe294d088e5
Shabhaizafar <zafarShabhai03@gmail.com>
2024-04-24 18:46:22 +0530

    AJAX INTRO

---
+++ AJAX/Ajax.js
@@ -0,0 +1,16 @@
+// function CollectData(){
+
+//     var ajax = new XMLHttpRequest();
+
+//     var url = "https://jsonplaceholder.typicode.com/todos/1";
+//     ajax.open("GET",url,true);
+
+//     ajax.onreadystatechange = function (){
+//         if(this.readyState == 4 && this.status==200)
+//         {
+//             console.log(this.responseText);
+//         }
+//     }
+//     ajax.send();
+// }
+// CollectData();
