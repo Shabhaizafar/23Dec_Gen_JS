11a1283d218d286b7da6d79bff494dbaff495665
Shabhaizafar <zafarShabhai03@gmail.com>
2024-05-11 16:54:53 +0530

    Regex

---
+++ AJAX/regex.js
@@ -0,0 +1,62 @@
+const btn = document.querySelector('button');
+const reg = /Z/g;
+btn.onclick = function (event){
+    const text = document.querySelector('input').value;
+    event.preventDefault();
+    // if(text.match(reg))
+    // {
+    //     var ans = text.match(reg);
+    //     console.log(ans);   
+    // }
+    // else{
+    //     var ans = text.match(reg);
+    //     console.log(ans);   
+    // }
+
+    // if(reg.test(text))
+    //     {
+    //         var ans = reg.test(text);
+    //         console.log(ans);   
+    //     }
+    //     else{
+    //         var ans = reg.test(text);
+    //         console.log(ans);   
+    //     }
+
+    // if(undefined)
+    // {
+    //     var ans = text.search(reg);
+    //     console.log(ans);   
+    // }
+    // else{
+    //     console.log("asdfg");   
+    // }
+
+    
+}
+
+
+
+
+
+
+// const fname = "12";
+// console.log(window.fname);
+// console.log(window);
+
+
+
+
+/*
+Match : 
+return : String Value /null
+
+test : 
+return : True/False
+
+Search : 
+return Index Value / -1
+
+*/ 
+
+
